d2dc964ec7ce0b5806de0e9ef9ee84a52efec94e
Sidh Khurana <sidhkhurana06@gmail.com>
2026-04-20 23:13:03 +0530

    Time: 34 ms (55.33%), Space: 88 MB (75.79%) - LeetHub

---
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        long long sum=0;
+        int score=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum>0){
+                score++;
+            }
+            else
+                break;
+        }
+        return score;
+    }
+};
