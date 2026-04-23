9fa23647f0857d205c28b4e102ffac7a766ef508
Sidh Khurana <sidhkhurana06@gmail.com>
2026-04-24 02:01:33 +0530

    Time: 0 ms (100%), Space: 13.7 MB (26.55%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<long long>st;
+        long long res=0,num=0;
+        int sign=1;
+        for(char c:s){
+            if(isdigit(c)){
+                num = num*10+(c-'0');
+            }
+            else if(c=='+'){
+                res+=sign*num;
+                num=0;
+                sign=1;
+            }
+            else if(c=='-'){
+                res+=sign*num;
+                num=0;
+                sign=-1;
+            }
+            else if(c=='('){
+                st.push(res);
+                st.push(sign);
+                res=0;
+                sign=1;
+            }
+            else if(c==')'){
+                res+=sign*num;
+                num=0;
+
+                res*=st.top();
+                st.pop();
+                res+=st.top();
+                st.pop();
+            }
+        }
+        res+=sign*num;
+        return (int)res;
+    }
+};
