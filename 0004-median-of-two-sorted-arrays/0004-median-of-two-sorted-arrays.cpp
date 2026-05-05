698f77b2b79f6ee9b6d31c9319ee12e4dbf54fdc
Sidh Khurana <sidhkhurana06@gmail.com>
2026-05-05 09:40:35 +0530

    Time: 0 ms (100%), Space: 95.1 MB (78.9%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -5,10 +5,7 @@ public:
         int total = n+m;
         int start = 0,i=0,j=0;
         double ans=0,prev,ite;
-        if(total%2==0)
-            ite=total/2 + 1;
-        else
-            ite = total/2+1;
+        ite = total/2+1;
         while(start<ite){
             prev = ans;
             if(i<n && (j==m || nums1[i]<=nums2[j])){
