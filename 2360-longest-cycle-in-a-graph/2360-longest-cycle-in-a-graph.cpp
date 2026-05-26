d8bacc4cb2e79da07e480cc7c202a717d4d72a22
Sidh Khurana <sidhkhurana06@gmail.com>
2026-05-27 02:45:16 +0530

    Time: 276 ms (17.17%), Space: 201.8 MB (18.67%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestCycle(vector<int>& edges) {
+        int n=edges.size();
+        vector<int>vis(n,0);
+        int ans=-1;
+
+        for(int i=0;i<n;i++){
+            unordered_map<int,int>mp;
+            int node=i;
+            int dist=0;
+            while(node!=-1 && !vis[node]){
+                vis[node] = 1;
+                mp[node] = dist++;
+                node = edges[node];
+                if(node!=-1 && mp.count(node))
+                    ans = max(ans,dist-mp[node]);
+            }
+        }
+        return ans;
+    }
+};
