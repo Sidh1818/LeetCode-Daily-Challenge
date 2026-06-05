00536eab1da2f6ca1d014e543d3a7c26230f1852
Sidh Khurana <sidhkhurana06@gmail.com>
2026-06-06 00:13:59 +0530

    Time: 0 ms (100%), Space: 23.7 MB (62.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        /*
         unordered_map<int,int>freq;
         for(int a:nums)
             freq[a]++;
@@ -10,6 +11,47 @@ public:
             if(a.second > occurence)
                 ans.push_back(a.first);
         }
+        return ans;
+        */
+        int candidate1 = 0 , candidate2 = 0;
+        int cnt1=0, cnt2=0;
+
+        for(int a:nums){
+            if(a == candidate1)
+                cnt1++;
+            else if(a == candidate2)
+                cnt2++;
+            else if(cnt1 == 0){
+                candidate1 = a;
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0){
+                candidate2 = a;
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+
+        cnt1=0,cnt2=0;
+        for(int a:nums){
+            if(a == candidate1)
+                cnt1++;
+            else if(a == candidate2)
+                cnt2++;
+        }
+
+        vector<int>ans;
+
+        if(cnt1 > nums.size()/3)
+            ans.push_back(candidate1);
+
+        if(cnt2 > nums.size()/3)
+            ans.push_back(candidate2);
+
         return ans;
     }
 };
