339606e27bd1887a59c2741a2a7c210cbbf65b9a
Sidh Khurana <sidhkhurana06@gmail.com>
2026-07-08 22:39:44 +0530

    Time: 182 ms (5.05%), Space: 11.9 MB (10.69%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool findright(vector<int>&have,vector<int>&needed){
+        for(int i=0;i<255;i++){
+            if(needed[i]>have[i])
+                return false;
+        }
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+        vector<int>needed(256,0);
+        vector<int>have(256,0);
+
+        for(char c:t)
+            needed[c]++;
+
+        int high=0,low=0,len,res=INT_MAX,strt=-1;
+
+        for(high=0;high<s.size();high++){
+            have[s[high]]++;
+
+            while(findright(have,needed)){
+                len = high-low+1;
+                if(res>len){
+                    res = len;
+                    strt = low;
+                } 
+                have[s[low]]--;
+                low++;
+            }
+        }
+        if(res == INT_MAX)
+            return "";
+        return s.substr(strt,res);
+    }
+};
